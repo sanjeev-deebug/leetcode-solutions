4640d1e4eac7bfb19473f310b387b4a5e70866f0
sanjeev-deebug <64364167+sanjeev-deebug@users.noreply.github.com>
2026-01-12 20:47:53 +0530

    Create Solution.java

---
+++ java/0001-two-sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (seen.containsKey(need)) {
+                return new int[]{seen.get(need), i};
+            }
+            seen.put(nums[i], i);
+        }
+        // As per problem statement, exactly one solution exists.
+        return new int[]{-1, -1};
+    }
+}
